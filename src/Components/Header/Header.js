c41320f667515e4a6703c0e1a654b5d0ad8e00c7
AdamKolodziejczyk <Adamkolodziejczyk59@gmail.com>
2021-10-26 10:36:14 +0200

    Change

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <p>fds</p>
+      <p>fds</p>
+      <p>fds</p>
+      <p>fds</p>
+    
+ 
+
+    </div>
+  );
+}
+
+export default Header;
